5f9a799a0f8e61ffa3820f1a0ceb7810b8a43f26
fsarrias <fsarrias@kolab.es>
2020-04-21 19:10:33 +0200

    pujo la fase 2

--- src/fase2.java
+++ src/fase2.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class fase2 {
+    public static void main(String[] args) {
+        List<Character> myname_list = new ArrayList<>();
+        myname_list.add('f');
+        myname_list.add('r');
+        myname_list.add('a');
+        myname_list.add('n');
+        myname_list.add('c');
+        myname_list.add('i');
+        myname_list.add('s');
+        myname_list.add('c');
+        myname_list.add('o');
+        myname_list.add(' ');
+        myname_list.add('s');
+        myname_list.add('a');
+        myname_list.add('r');
+        myname_list.add('r');
+        myname_list.add('i');
+        myname_list.add('a');
+        myname_list.add('s');
+        // Loop for printing characters
+        for (char letra : myname_list) {
+           if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o'|| letra == 'u') {
+            System.out.println(letra + " es una VOCAL");}
+           else {
+               if (letra == '1' || letra == '2' || letra == '3' || letra == '4'|| letra == '5' || letra == '6' || letra == '7' || letra == '8' || letra == '9'|| letra == '0') {
+                   System.out.println(letra + " person names do not include NUMBERS");}
+               else { System.out.println(letra + " es una CONSONANT"); }
+        }
+    }
+    }
 }

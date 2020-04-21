fb67efa5dfaef9cd0d472981a351dbb0c9f33bbb
fsarrias <fsarrias@kolab.es>
2020-04-21 17:45:01 +0200

    pujo la fase 1

---
+++ src/fase1.java
@@ -0,0 +1,10 @@
+public class fase1 {
+        public static void main(String[] args) {
+            // Use array initializer syntax.
+            char[] myname_array = { 'f', 'r', 'a', 'n', 'c', 'i', 's', 'c', 'o', ' ', 's', 'a', 'r', 'r', 'i', 'a', 's' };
+            // Loop for printing characters
+            for (int i=0; i<myname_array.length; i++){
+                System.out.println(myname_array[i]);
+            }
+        }
+    }
